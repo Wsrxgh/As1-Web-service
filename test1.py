fd0a511c5ae66586760799bb7d83b638c1f59c59
cicacica5 <stefanieliu0505@gmail.com>
2024-02-12 12:21:47 +0100

    edit parser

--- test1.py
+++ test1.py
@@ -1,96 +1,198 @@
-from flask import Flask, request, jsonify, redirect, abort
-import hashlib
+import unittest
+import requests
 import json
-import threading
-import re
 
-app = Flask(__name__)
 
-url_mapping = {}# 这个字典将存储短URL到原始URL的映射
-id_to_url = {}
-next_id = 1  # 初始化自增ID
+class TestApi(unittest.TestCase):
+    #with fastapi default port is 8000 with flask is 5000
+    base_url = "http://127.0.0.1:5000"
+    
 
-def is_valid_url(url):
-    # 此正则表达式检查大部分常见的URL格式
-    regex = re.compile(
+
+    def setUp(self):
+        # populate data before each test by doing two POST
+        #TODO make it read data from a csv or similar to avoid people hardcoding answers
+        self.url_to_shorten_1="https://en.wikipedia.org/wiki/Docker_(software)"
+        self.url_to_shorten_2="https://fastapi.tiangolo.com"
+        self.id_shortened_url_1=""
+        self.id_shortened_url_2=""
         
-        r'^(https?|ftp):\/\/'  
-        r'((?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+'  
-        r'(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  
-        r'localhost|'  
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  
-        r'(?::\d+)?'  
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)  
-    return re.match(regex, url) is not None
-  
-def base62_encode(num): #基62编码
-    characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    base = len(characters)
-    encoded = ''
-    while num > 0:
-        num, rem = divmod(num, base)
-        encoded = characters[rem] + encoded
-    return encoded
 
-@app.route('/', methods=['POST']) #创建映射
-def create_url():
-    global next_id
-    data = request.get_json()
-    if 'url' not in data or not is_valid_url(data['url']):
-        return jsonify({'error': 'Invalid URL'}), 400
+        def do_post(url_to_shorten):
+            endpoint = "/"       
+            print(url_to_shorten)
+            url = f"{self.base_url}{endpoint}"   
+            response = requests.post(url, json={'value': str(url_to_shorten)})
+            self.assertEqual(response.status_code, 201, f"Expected status code 201, but got {response.status_code}")
+            self.assertIsNotNone(response.text, "Response text should not be None.")
+            response_extracted=response.json()
+            id_returned=response_extracted["id"]#TODO add try catch / handle in case no id key was returned
+            return id_returned
+ 
+        self.id_shortened_url_1=do_post(self.url_to_shorten_1)
+        print("id 1 obtained "+str(self.id_shortened_url_1))
+        self.id_shortened_url_2=do_post(self.url_to_shorten_2)
+        print("id 2 obtained "+str(self.id_shortened_url_2))
+      
+
+    def tearDown(self): #OK
+        #erase everything
+        endpoint = "/"
+        url = f"{self.base_url}{endpoint}"
+        response = requests.delete(url)
+        #self.assertEqual(response.status_code, 404, f"Expected status code 404 to confirm correct erase, but got {response.status_code}")
+
     
-    original_url = data['url']
-    if original_url in id_to_url:
-            # 如果URL已存在，直接返回对应的ID
-            short_id = id_to_url[original_url]
-    else:
-            # 否则，创建一个新的映射
-            short_id = base62_encode(next_id)
-            url_mapping[short_id] = original_url
-            id_to_url[original_url] = short_id  # 保存URL到ID的映射
-            next_id += 1
+    """
+    /:id GET  
+    Given an ID to the service, redirects the user to the matching long URL by returning 301 with the URL. 
+    If the ID does not exist, 404 should be returned.
+    """
+    def test_get_request_with_id_expect_301(self):
+        
+        id = self.id_shortened_url_1
+        expected_value = self.url_to_shorten_1
+
+        endpoint = "/"
+        url = f"{self.base_url}{endpoint}{id}"
+        response = requests.get(url)
+        
+        self.assertEqual(response.status_code, 301, f"Expected status code 301, but got {response.status_code}")
+        
+        self.assertEqual(response.json().get("value"), expected_value, "Expected response body to be " +expected_value+" , but got "+response.json().get("value"))
+
+    def test_get_request_with_id_expect_404(self):
+        
+        id = "Unseen_id" 
+
+        endpoint = "/"
+        url = f"{self.base_url}{endpoint}{id}"
+        response = requests.get(url)
+        
+        self.assertEqual(response.status_code, 404, f"Expected status code 404, but got {response.status_code}")
     
-    return jsonify({'id': short_id}), 201
+    """
+    /:id PUT 
+    Updates the URL behind the given ID. To do so, in addition to the parameter in the URL, 
+    the service requires additional information in the body of the request (e.g., encoded as JSON) 
+    that contains the new version of the URL. Then, the function returns 200 if the update was successful, 400 with an error 
+    if the update failed (e.g., the URL was invalid) or 404 if the ID does not exist.
+    """
+    def test_put_id(self):
 
-@app.route('/', methods=['DELETE'])
-def delete_not_supported():
-    abort(404)
+        id = self.id_shortened_url_1
+        url_to_update = self.id_shortened_url_1
+        url_after_update = "https://en.wikipedia.org/wiki/Cypherpunk"
+        not_existing_id = "not_existing_id"
+        invalid_url = "pip_install_requests"
 
-@app.route('/', methods=['GET'])
-def list_urls():
-    # 列出所有映射的键
-    keys = list(url_mapping.keys())
-    return jsonify(keys), 200
+        endpoint = "/"
+        url = f"{self.base_url}{endpoint}{id}"   
+        response = requests.put(url, data=json.dumps({'url': url_after_update}))
+        self.assertEqual(response.status_code, 200, f"Expected status code 200, but got {response.status_code}")
 
-@app.route('/<short_id>', methods=['GET'])
-def redirect_to_url(short_id):
-    # 根据提供的短ID重定向到URL
-    original_url = url_mapping.get(short_id)
-    if original_url:
-        return redirect(original_url, code=301)
-    else:
-        abort(404)
+        #check value has been rally changed by doing a get
+        url = f"{self.base_url}{endpoint}{id}"
+        response = requests.get(url)
+        self.assertEqual(response.json().get("value"), url_after_update, "Expected response body to be " +url_after_update+" , but got "+response.json().get("value"))
 
-@app.route('/<short_id>', methods=['PUT'])
-def update_url(short_id):
-    # 更新一个已存在的URL映射
-    data = request.get_json()
-    if 'url' not in data:
-        return jsonify({'error': 'Error'}), 400
-    if short_id in url_mapping:
-        url_mapping[short_id] = data['url']
-        return jsonify({}), 200
-    else:
-        abort(404)
+        
+        #check 400 by passing invalid url
+        url = f"{self.base_url}{endpoint}{id}"   
+        response = requests.put(url, data=json.dumps({'url': invalid_url}))
+        self.assertEqual(response.status_code, 400, f"Expected status code 400, but got {response.status_code}")
 
-@app.route('/<short_id>', methods=['DELETE'])
-def delete_url(short_id):
-    # 删除一个URL映射
-    if short_id in url_mapping:
-        del url_mapping[short_id]
-        return jsonify({}), 204
-    else:
-        abort(404)
+        #check 404 aka if id exists
+        url = f"{self.base_url}{endpoint}{not_existing_id}"   
+        response = requests.put(url, data=json.dumps({'url': not_existing_id}))
+        self.assertEqual(response.status_code, 404, f"Expected status code 404, but got {response.status_code}")
+        
+
+    """
+    /:id DELETE 
+    Deletes the given short URL / ID and then returns 204. If the ID does not exist, returns 404
+    """
+    def test_deletion_id(self):
+        #Erase ID
+        endpoint = "/"
+        id = self.id_shortened_url_1
+        #url = f"{self.base_url}{endpoint}?value={id}"   
+        url = f"{self.base_url}{endpoint}{id}"   
+        response = requests.delete(url)   
+        self.assertEqual(response.status_code, 204, f"Expected status code 204, but got {response.status_code}")
+        response = requests.delete(url)
+        self.assertEqual(response.status_code, 404, f"Expected status code 404, but got {response.status_code}")
+
+
+    """
+    / GET   
+    Should return a list of something at the global level. Can either be a list of all keys (IDs), all long URLs, 
+    or some combination of the two. Should return 200 on success, but the students can choose what to do on failure 
+    (if applicable)
+    """
+    def test_get_all(self):
+        endpoint = "/"        
+        url = f"{self.base_url}{endpoint}"        
+        response = requests.get(url)        
+        self.assertEqual(response.status_code, 200, f"Expected status code 200, but got {response.status_code}")
+        self.assertIsNotNone(response.text, "Response text should not be None.")
+    
+
+    """
+    / POST    
+    Should create a new short URL / ID for the given long URL (which must be given in the body). Returns a 201 with 
+    the new ID on success, or a 400 if there was some failure (e.g., missing URL in body or URL was invalid).
+    """
+    def test_post(self):
+        #add link 
+        url_to_shorten="https://en.wikipedia.org/wiki/Docker_(software)"
+        endpoint = "/"    
+
+        #url = f"{self.base_url}{endpoint}?value={url_to_shorten}"   
+        #response = requests.post(url)
+
+        url = f"{self.base_url}{endpoint}"   
+        response = requests.post(url, json={'value': str(url_to_shorten)})
+
+        
+        self.assertEqual(response.status_code, 201, f"Expected status code 201, but got {response.status_code}")
+        self.assertIsNotNone(response.json().get("id"), "Response text should not be None.")
+
+        
+        #check if id returned is correct
+        temp=response.json().get("id")
+        url = f"{self.base_url}{endpoint}{temp}"
+        response = requests.get(url)
+        self.assertEqual(response.status_code, 301, f"Expected status code 301, but got {response.status_code}")
+        self.assertEqual(response.json().get("value"), url_to_shorten, "Expected response body to be " +url_to_shorten+" , but got "+response.json().get("value"))
+
+
+        #try error by passing an empty link
+        url_to_shorten=""
+        endpoint = "/"       
+        url = f"{self.base_url}{endpoint}{url_to_shorten}"   
+        response = requests.post(url, json={'value': str(url_to_shorten)})
+        self.assertEqual(response.status_code, 400, f"Expected status code 400, but got {response.status_code}")
+       
+        
+    
+    """
+    / DELETE    
+    Deletes all ID/URL pairs in the service.
+    """
+    def test_deletion_all(self):
+        #delete all
+        endpoint = "/"       
+        url = f"{self.base_url}{endpoint}"   
+        response = requests.delete(url)
+        self.assertEqual(response.status_code, 404, f"Expected status code 404 to confirm correct erase, but got {response.status_code}")
+
+        #make a getall to check that stuff is erased
+        endpoint = "/"
+        url = f"{self.base_url}{endpoint}"
+        response = requests.get(url)
+        self.assertIsNone(response.json().get("value"), "The value should be None since should be empty.")
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    unittest.main()
