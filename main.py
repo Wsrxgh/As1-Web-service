fd12c605e07bad672020b28154789d7811a234b2
Wsrxgh <150687797+Wsrxgh@users.noreply.github.com>
2024-02-12 21:08:45 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -13,15 +13,14 @@ next_id = 1  # 初始化自增ID
 def is_valid_url(url):
     # 此正则表达式检查大部分常见的URL格式
     regex = re.compile(
-
-        r'^(https?|ftp):\/\/'
-        r'((?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+'
-        r'(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
-        r'localhost|'
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
-        r'(?::\d+)?'
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-
+        
+        r'^(https?|ftp):\/\/'  
+        r'((?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+'  
+        r'(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  
+        r'localhost|'  
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  
+        r'(?::\d+)?'  
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)  
     return re.match(regex, url) is not None
   
 def base62_encode(num): #基62编码
@@ -37,22 +36,23 @@ def base62_encode(num): #基62编码
 def create_url():
     global next_id
     data = request.get_json()
-
     if 'value' not in data or not is_valid_url(data['value']):
         return jsonify({'error': 'Invalid URL'}), 400
     
-    original_url = data['value']
-    if original_url in id_to_url:
+    url = data['value']
+    if url in id_to_url and id_to_url[url] in url_mapping:
             # 如果URL已存在，直接返回对应的ID
-            short_id = id_to_url[original_url]
+            id = id_to_url[url]
     else:
             # 否则，创建一个新的映射
-            short_id = base62_encode(next_id)
-            url_mapping[short_id] = original_url
-            id_to_url[original_url] = short_id  # 保存URL到ID的映射
+            id = base62_encode(next_id)
+            url_mapping[id] = url
+            id_to_url[url] = id  # 保存URL到ID的映射
             next_id += 1
     
-    return jsonify({'id': short_id}), 201
+    return jsonify({'id': id}), 201
+
+
 
 @app.route('/', methods=['DELETE'])
 def delete_not_supported():
@@ -62,49 +62,37 @@ def delete_not_supported():
 def list_urls():
     # 列出所有映射的键
     keys = list(url_mapping.keys())
-    return jsonify(keys), 200
+    return jsonify({"values": keys}), 200
 
-@app.route('/<short_id>', methods=['GET'])
-def redirect_to_url(short_id):
+@app.route('/<id>', methods=['GET'])
+def redirect_to_url(id):
     # 根据提供的短ID重定向到URL
-    original_url = url_mapping.get(short_id)
-    if original_url:
-        return redirect(original_url, code=301)
+    url = url_mapping.get(id)
+    if url:
+        return redirect(url, code=301)
     else:
         abort(404)
 
-@app.route('/<short_id>', methods=['PUT'])
-def update_url(short_id):
+@app.route('/<id>', methods=['PUT'])
+def update_url(id):
     # 更新一个已存在的URL映射
     data = request.get_json()
-    if 'value' not in data:
+    if 'url' not in data:
         return jsonify({'error': 'Error'}), 400
-    if short_id in url_mapping:
-        url_mapping[short_id] = data['value']
+    if id in url_mapping:
+        url_mapping[id] = data['url']
         return jsonify({}), 200
     else:
         abort(404)
 
-@app.route('/<short_id>', methods=['DELETE'])
-def delete_url(short_id):
+@app.route('/<id>', methods=['DELETE'])
+def delete_url(id):
     # 删除一个URL映射
-    if short_id in url_mapping:
-        del url_mapping[short_id]
+    if id in url_mapping:
+        del url_mapping[id]
         return jsonify({}), 204
     else:
         abort(404)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-'''
-    regex = re.compile(
-
-        r'^(https?|ftp):\/\/'
-        r'((?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+'
-        r'(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
-        r'localhost|'
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
-        r'(?::\d+)?'
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-'''
